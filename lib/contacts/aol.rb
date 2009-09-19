cb038bd4c55d11dea267e6a2fe04cee3dc658c2e
Rodolfo Carvalho <rodolfo@rcarvalho.eti.br>
2009-09-19 10:11:41 -0300

    Added a basic contacts fetch request.

--- lib/contacts/aol.rb
+++ lib/contacts/aol.rb
@@ -1,4 +1,5 @@
 # Author: Rodolfo Carvalho <rodolfo@nybusinesslink.com>
+AR_PRESENT = defined?(ActiveSupport::JSON) and ActiveSupport::JSON.respond_to?(:decode)
 
 require 'contacts'
 
@@ -10,11 +11,14 @@ require 'zlib'
 require 'stringio'
 require 'net/http'
 require 'net/https'
+require 'uri'
+require json unless AR_PRESENT
 
 module Contacts
   class AOL
     CONFIG_FILE = File.dirname(__FILE__) + '/../config/contacts.yml'
-    LOGIN_URL = "http://api.screenname.aol.com/auth/login?f=qs&r=27&devId=#id&succUrl=#url&supportedIdType=#sid"
+    LOGIN_URL = "http://api.screenname.aol.com/auth/login?f=qs&devId=#id&succUrl=#url&supportedIdType=#sid"
+    PRESENCE_URL = "http://api.oscar.aol.com/presence/get?f=json&bl=1&k=#id&a=#token"
 
     def initialize(config=CONFIG_FILE)
       confs = YAML::load_file(config)['aol']
@@ -29,6 +33,27 @@ module Contacts
       url.gsub!(/#url/, CGI.escape(@return_url))
       url.gsub!(/#sid/, @supported_types.join(","))
       url
+    end
+    
+    def contacts(token)
+      url = PRESENCE_URL.dup
+      url.gsub!(/#id/, @dev_id)
+      url.gsub!(/#token/, token)
+      uri = URI.parse(url)
+      response = Net::HTTP::Request.start(uri.host, uri.port) do |http|
+        http.get("#{uri.path}?#{uri.query}")
+      end
+      
+      if response.kind_of?(Net::HTTPSuccess)
+        body = if AR_PRESENT
+          ActiveSupport::JSON.decode(response.body)
+        else
+          JSON.loads(response.body)
+        end
+        return body
+      else
+        return "An Error has occurrend: #{response.class}"
+      end
     end
   end
 end
